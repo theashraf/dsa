e3a7c331b615adda220f891f84904fe5357baf3d
Abdelrahman Ashraf <a.theashraf@gmail.com>
2019-05-10 15:29:50 +0200

    add Monotonic Stack

---
+++ Stack/MonotonicStack.ts
@@ -0,0 +1,42 @@
+import Stack from "./Stack";
+import LinkedListStack from "./LinkedListStack";
+
+export default class MonotonicStack implements Stack<number> {
+  private maxStack: Stack<number>;
+  private minStack: Stack<number>;
+  private stack: Stack<number>;
+
+  constructor() {
+    this.maxStack = new LinkedListStack();
+    this.minStack = new LinkedListStack();
+    this.stack = new LinkedListStack();
+  }
+
+  push(data: number) {
+    this.stack.push(data);
+    if (this.maxStack.isEmpty()) this.maxStack.push(data);
+    else if (data >= this.maxStack.top()) this.maxStack.push(data);
+    if (this.minStack.isEmpty()) this.minStack.push(data);
+    else if (data <= this.minStack.top()) this.minStack.push(data);
+  }
+  pop(): number {
+    if (this.stack.top() === this.maxStack.top()) this.maxStack.pop();
+    if (this.stack.top() === this.minStack.top()) this.minStack.pop();
+    return this.stack.pop();
+  }
+  top(): number {
+    return this.stack.top();
+  }
+  isEmpty(): boolean {
+    return this.stack.isEmpty();
+  }
+  size(): number {
+    return this.stack.size();
+  }
+  max(): number {
+    return this.maxStack.top();
+  }
+  min(): number {
+    return this.minStack.top();
+  }
+}
