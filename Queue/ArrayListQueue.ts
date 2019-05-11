d6948d67b544a0826cbfcc6dc893134a8c23e683
Abdelrahman Ashraf <a.theashraf@gmail.com>
2019-05-12 00:19:41 +0200

    add ArrayListQueue

---
+++ Queue/ArrayListQueue.ts
@@ -0,0 +1,37 @@
+import Queue from "./Queue";
+import { DynamicArrayList as ArrayList } from "../List/ArrayList";
+
+export default class ArrayListQueue<T> implements Queue<T> {
+  private list: ArrayList<T>;
+
+  constructor(capacity?: number) {
+    this.list = new ArrayList(capacity);
+  }
+
+  // O(1)
+  enqueue(data: T): void {
+    this.list.add(this.list.size(), data);
+  }
+
+  // O(n)
+  dequeue(): T {
+    if (this.isEmpty()) return null;
+    return this.list.remove(0);
+  }
+
+  // O(1)
+  first(): T {
+    if (this.list.isEmpty()) return null;
+    return this.list.get(0);
+  }
+
+  // O(1)
+  size(): number {
+    return this.list.size();
+  }
+
+  // O(1)
+  isEmpty(): boolean {
+    return this.list.isEmpty();
+  }
+}
